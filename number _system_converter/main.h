7d37ea330f05f522b075caa4aa6c9da89528551b
a.pashkova <a.pashkova@g.nsu.ru>
2022-09-28 20:57:51 +0700

    Fixed some sintax/First day of Olesyas challenge

--- number _system_converter/main.h
+++ number _system_converter/main.h
@@ -1,9 +1,9 @@
 void Lab0();
-int CheckStr(char* X, int b1);
-void ConvertToValue(char *chis, int b1, int b2);
+int CheckStr(char *X, int b1);
+void ConvertToValue(char *value, int b1, int b2);
 void ConvertToB2(long long int *numerator, long long int denominator, int b2);
-void ReflectStr(char* ans, char* ans_rev, int i);
+void ReflectStr(char *ans, char *ans_rev, int i);
+int IsNotValidBase(int b1, int b2);
 void Error();
-char DigitToStr(long long int chis);
-long long int CharToValue(char *chis, long long int *numerator, int b1);
-
+char DigitToStr(long long int value);
+long long int CharToValue(char *value, long long int *numerator, int b1);
