a8ec00c4fa42b51b091904757fce6799c89e139a
Alina Pashkova <87114350+Just0Lina@users.noreply.github.com>
2022-10-01 22:59:18 +0700

    Create main.h

---
+++ Naarayana_algorithm/main.h
@@ -0,0 +1,10 @@
+#define NMAX 10
+#define BIGGEST -1
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+void lab2();
+int Narayana(char str[], int strl);
+void Error();
+void Swap(char *a, char *b);
+void ReverseStr(char ans[], int left, int right);
